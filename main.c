809e13a419013740d7fba814b56153c9754ff178
Josh Caplinger <jcaplinger93@gmail.com>
2016-10-30 19:18:52 -0700

    added Makefile and made minor changes

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: nengle- <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/23 23:02:34 by nengle-           #+#    #+#             */
-/*   Updated: 2016/10/27 14:11:01 by nengle-          ###   ########.fr       */
+/*   Updated: 2016/10/30 19:09:34 by jcapling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ char *read_file(char *file)
 	i = 0;
 	if ((fd = open(file, O_RDONLY)) == -1)
 		ft_putstr("error\n");
-	while (ret = read(fd, buf, 1))
+	while ((ret = read(fd, buf, 1)))
 	{
 		tmp[i++] = buf[0];
 		if (i > 545)
@@ -32,21 +32,30 @@ char *read_file(char *file)
 	tmp[i] = '\0';
 	if (close(fd) == -1)
 		ft_putstr("error\n");
-	return (tmp);
+	return (ft_strdup(tmp));
 }
 
-int main(int ac, char **av)
+void	ft_error(void)
 {
-	char *tetriminos;
-	char **map;
+	write(1, "error\n", 6);
+	exit (0);
+}
 
-	tetriminos = read_file(av[1]);
-	if (!check_valid(tetriminos))
-	{
-		ft_putstr("error\n");
-		return (0);
-	}
-	map = make_map(tetriminos);
+int 	main(int ac, char **av)
+{
+	//char *tetriminos;
+//	char **map;
+
+	(void)ac;
+	(void)av;
+
+//	tetriminos = read_file(av[1]);
+//	if (!check_valid(tetriminos))
+//	{
+//		ft_putstr("error\n");
+//		return (0);
+//	}
+//	map = make_map(tetriminos);
 	
 	
 	return (0);
